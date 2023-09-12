3f8b8dc0aeae5df686bc79c6d8c11de759f023d2
samuelraka <samuelrakayustianto@gmail.com>
2023-09-12 18:19:09 +0700

    Update index,script
    pengurangan pertanyaan dan perubahan pada script js

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 
 // more button
 function showMoreQuestions() {
-    const hiddenQuestions = document.querySelectorAll('.question-box.hidden');
-
-    for (let i = 0; i < 3 && i < hiddenQuestions.length; i++) {
-        hiddenQuestions[i].style.display = 'block';
-        hiddenQuestions[i].classList.remove('hidden');
-    }
-
-    if (document.querySelectorAll('.question-box.hidden').length === 0) {
-        document.querySelector('.more-button').style.display = 'none';
-    }
+    window.location.href = "pertanyaan.html";
 }
 
 function toggleAnswer(questionNumber) {
