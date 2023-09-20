4bc6d4306c8438a02351e42b79e497e9364512fa
jocjoc <garox.javar@gmail.com>
2023-09-21 00:12:17 +0700

    update halaman qna dan accordion

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function toggleAnswer(questionNumber) {
         answer.style.display = 'none';
     }
 }
+
+//whats
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("activeAccordion");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
